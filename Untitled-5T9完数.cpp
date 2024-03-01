ebadba8afd0aebc70fdb2f54bb22cce464bbf36e
fw-Summers <161791338+fw-Summers@users.noreply.github.com>
2024-03-01 11:06:18 +0800

    Update Untitled-5T9完数.cpp
    注释

--- Untitled-5T9完数.cpp
+++ Untitled-5T9完数.cpp
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include<stdio.h>//求1000以内的所有完数（一个数恰好等于其因子之和）
 int main()
 {
 	int n,i,m;
 	for(n=2;n<=1000;n++){
 		m=0;
 		for(i=1;i<=n/2;i++)
-			if(n%i==0)m+=i;
+			if(n%i==0)m+=i;//m代表所有因子之和
 		if(m==n){
-			printf("%d its factors are 1",n);
+			printf("%d its factors are 1",n);//1是所有完数的因子
 			for(i=2;i<=n/2;i++)
-				if(n%i==0)printf(",%d",i);
+				if(n%i==0)printf(",%d",i);//继续列出1以外的所有因子
 			printf("\n");
 		}
 	}
